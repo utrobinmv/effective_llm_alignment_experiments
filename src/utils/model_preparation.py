1b22ebbece6c703252375b8e39ff6a919befe7be
Mikhail Utrobin <utrobinmv@yandex.ru>
2025-10-30 12:35:48 +0000

    Исправил ошибка с custom_pad_token. Из за неё не училось.

--- src/utils/model_preparation.py
+++ src/utils/model_preparation.py
@@ -47,8 +47,6 @@ def setup_model_and_tokenizer(
             {"additional_special_tokens": args.added_special_tokens}
         )
         model.resize_token_embeddings(len(tokenizer))
-    # add joefox
-    tokenizer.pad_token = tokenizer.eos_token
 
 
 def unfreeze_modules_by_patterns(model, patterns):
